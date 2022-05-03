d4067f8ddda95c6bd3c2ac2f7a1b1dadc0c0893d
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-05-03 18:06:26 +0900

    Update Loader.php

--- src/authorname/pluginname/Loader.php
+++ src/authorname/pluginname/Loader.php
@@ -5,6 +5,8 @@ namespace authorname\pluginname;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\BlockBreakEvent;
+use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\entity\object\ItemEntity;
 final class Loader extends PluginBase{
     protected function onLoad() : void{
         
@@ -21,5 +23,10 @@ final class Loader extends PluginBase{
         foreach($event->getDrops() as $key => $item){
             $item->kill();    
         }
+    }
+　　 public function ItemSpawn(EntitySpawnEvent $event){
+　　　　　if(!$event->getEntity() instanceof ItemEntity) return;
+        $itemEntity = $event->getEntity();
+	$itemEntity->kill();
     }
 }
